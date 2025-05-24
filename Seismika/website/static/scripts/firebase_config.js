1cdbab02d21a602ff00da0d83c4ec2553ca81d88
HenrikLaidvee <158087294+HenrikLaidvee@users.noreply.github.com>
2025-05-24 19:48:02 +0300

    Update firebase_config.js

--- Seismika/website/static/scripts/firebase_config.js
+++ Seismika/website/static/scripts/firebase_config.js
@@ -1,9 +1,4 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCyMAM3CUzG4eLtfk_h9ixp1hS2Bn9_dpI",
-  authDomain: "seimika-veebileht.firebaseapp.com",
-  projectId: "seimika-veebileht",
-  storageBucket: "seimika-veebileht.appspot.com",
-  messagingSenderId: "789822339227",
-  appId: "1:789822339227:web:90a2620766a3e0c72d2e3e"
+
 };
 firebase.initializeApp(firebaseConfig);
